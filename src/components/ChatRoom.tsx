99f317cbf4513f9ec015a875f8250445280892ad
Ewostatewos18 <nebyuehitemusie@gmail.com>
2025-04-07 11:12:48 +0300

    modified

--- src/components/ChatRoom.tsx
+++ src/components/ChatRoom.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Auth from "./Auth/Auth";
 import Chat from "./Chat/chat";
 import { getAuth } from "firebase/auth";
--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { getDatabase, ref, onValue } from 'firebase/database';
 import { getAuth } from 'firebase/auth';
 
